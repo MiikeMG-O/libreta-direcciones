95b0088799ba6adfc65a1e95094c3f2bf1a52edd
Miguel Angel GV <miguel.godinez.villarruel@gmail.com>
2024-09-17 13:43:57 -0600

    Integración de Angular y rutas en Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{any}', [FrontController::class, 'index'])->where('any', '.*');
